952cfaf9f99159da1d77f990ceaf7f9131b247ba
potato-lulw <om.patil17072@gmail.com>
2024-04-15 01:46:16 +0530

    Initial commit

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <button onClick={()=>{console.log('hello')}}>Button</button>
+    </div>
+  )
+}
+
+export default ProductCard
