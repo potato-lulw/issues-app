3c0ff168e953471ce0402a7e3d216076b5ec4d7c
potato-lulw <om.patil17072@gmail.com>
2024-04-26 20:15:43 +0530

    Registering users

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,6 +2,7 @@ import prisma from "@/prisma/client";
 import Email from "next-auth/providers/email";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import bcrypt from "bcrypt"
 
 const schema = z.object({
     email: z.string().email(),
@@ -17,7 +18,21 @@ export async function POST(request: NextRequest) {
         return NextResponse.json({error: validate.error.errors}, {status: 400});
     } 
 
-    prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
         where: {email: body.email},
     })
+
+    if(user) {
+        return NextResponse.json({error: 'User already exists'}, {status: 400});
+    }
+
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+    const newUser = await prisma.user.create({  
+        data: {
+            email: body.email,
+            hashedPassword: hashedPassword,
+        }
+    })
+
+    return NextResponse.json(newUser, {status: 201});
 }
